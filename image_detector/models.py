fe52faee503667b78d98c1cc54995ed4200026c2
Hendra Wardana <maxnara19@gmail.com>
2021-04-14 21:46:06 +0700

    add activemanager to get status=1 data

--- image_detector/models.py
+++ image_detector/models.py
@@ -2,20 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class ActiveManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset().filter(status=1)
+
 class Plan(models.Model):
   name = models.CharField(max_length=500)
   price = models.DecimalField(max_digits=24, decimal_places=2)
   status = models.BooleanField(default=True)
+
+  objects = ActiveManager()
 
   def __str__(self):
     return self.name
 
 class Subscription(models.Model):
   username = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-  plan_id = models.ForeignKey(Plan, on_delete=models.DO_NOTHING)
+  plan = models.ForeignKey(Plan, on_delete=models.DO_NOTHING)
   start_at = models.DateField(default=timezone.now)
   end_at = models.DateField(default=timezone.now)
   status = status = models.BooleanField(default=True)
+
+  objects = ActiveManager()
 
 class Detection(models.Model):
   subs_id = models.ForeignKey(Subscription, on_delete=models.DO_NOTHING)
