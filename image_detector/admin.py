7070098ef55b1a120a83fa23fc9cd1b14acd9d95
Hendra Wardana <maxnara19@gmail.com>
2021-05-10 12:12:54 +0700

    add detection to admin

--- image_detector/admin.py
+++ image_detector/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Plan, Subscription
+from .models import Plan, Subscription, Detection
 
 admin.site.register(Plan)
 admin.site.register(Subscription)
+admin.site.register(Detection)
