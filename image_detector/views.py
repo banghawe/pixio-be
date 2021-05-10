3a3543414216f6c4173f40d6934f1bab7f16d288
Hendra Wardana <maxnara19@gmail.com>
2021-05-10 12:15:03 +0700

    add list retrieve and upload method

--- image_detector/views.py
+++ image_detector/views.py
@@ -1,11 +1,12 @@
 import sys
 
 from rest_framework import viewsets, status
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from .models import Plan, Subscription
-from .serializers import PlanSerializer, SubscriptionSerializer
+from .models import Plan, Subscription, Detection
+from .serializers import PlanSerializer, SubscriptionSerializer, DetectionSerializer
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -67,3 +68,53 @@ class SubscriptionViewSet(viewsets.ViewSet):
         except:
             print(sys.exc_info())
             return Response({"message": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class DetectionViewSet(viewsets.ViewSet):
+    parser_classes = (MultiPartParser, FormParser)
+
+    @staticmethod
+    def list(request: Request, username: str) -> Response:
+        try:
+            user = User.objects.get(username=username)
+            detections = Detection.objects.filter(created_by=user.id)
+            serializer = DetectionSerializer(detections, many=True)
+
+            return Response(serializer.data)
+        except User.DoesNotExist:
+            return Response({"message": "User has not found"}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            print(sys.exc_info())
+            return Response({"message": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @staticmethod
+    def retrieve(request: Request, username: str, detection_id: int) -> Response:
+        try:
+            detection = Detection.objects.get(pk=detection_id)
+            serializer = DetectionSerializer(detection)
+
+            return Response(serializer.data)
+        except Detection.DoesNotExist:
+            return Response({"message": "Detection has not found"}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({"message": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @staticmethod
+    def upload(request: Request, username: str) -> Response:
+        try:
+            serializer = DetectionSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except:
+            print(sys.exc_info())
+            return Response({"message": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @staticmethod
+    def detect(request: Request, username: str, detection_id: int) -> Response:
+        pass
+
+    @staticmethod
+    def progress(request: Request, detection_id: int) -> Response:
+        pass
