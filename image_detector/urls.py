fae5dacfd9a7085ac52a5f23cde3ecb30e8207af
Hendra Wardana <maxnara19@gmail.com>
2021-05-10 12:14:16 +0700

    add detection endpoint

--- image_detector/urls.py
+++ image_detector/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import PlanViewSet, SubscriptionViewSet
+from .views import PlanViewSet, SubscriptionViewSet, DetectionViewSet
 
 urlpatterns = [
     path('plan', PlanViewSet.as_view({
@@ -12,5 +12,16 @@ urlpatterns = [
     })),
     path('subscription/<str:user_id>', SubscriptionViewSet.as_view({
         'get': 'retrieve',
+    })),
+    path('detection/<str:username>', DetectionViewSet.as_view({
+        'get': 'list',
+        'post': 'upload',
+    })),
+    path('detection/<str:username>/<str:detection_id>', DetectionViewSet.as_view({
+        'get': 'retrieve',
+        'patch': 'detect',
+    })),
+    path('detection/<str:username>/<str:task_id>/progress', DetectionViewSet.as_view({
+        'get': 'progress',
     })),
 ]
