da5d48cd544098cec044988499161e0d9b8c5680
Hendra Wardana <maxnara19@gmail.com>
2021-05-16 01:24:00 +0700

    add login using jwt

--- image_detector/urls.py
+++ image_detector/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
-from .views import PlanViewSet, SubscriptionViewSet, DetectionViewSet
+from .views import PlanViewSet, SubscriptionViewSet, DetectionViewSet, RegisterView
 
 urlpatterns = [
     path('plan', PlanViewSet.as_view({
@@ -10,15 +15,13 @@ urlpatterns = [
     })),
     path('subscription', SubscriptionViewSet.as_view({
         'post': 'create',
-    })),
-    path('subscription/<str:user_id>', SubscriptionViewSet.as_view({
         'get': 'retrieve',
     })),
-    path('detection/<str:username>', DetectionViewSet.as_view({
+    path('detection', DetectionViewSet.as_view({
         'get': 'list',
         'post': 'upload',
     })),
-    path('detection/<str:username>/<int:detection_id>', DetectionViewSet.as_view({
+    path('detection/<int:detection_id>', DetectionViewSet.as_view({
         'get': 'retrieve',
         'patch': 'detect',
     })),
@@ -28,4 +31,8 @@ urlpatterns = [
     path('docs/', TemplateView.as_view(
         template_name='api-docs.html',
     ), name='api-docs'),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('register', RegisterView.as_view(), name='auth_register')
 ]
